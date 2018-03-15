1a8b211a9801f61fb2597c5f68ddf54d19d60f9c
Jonathan Elliott <jonathan.elliott1247@gmail.com>
2018-03-14 18:26:11 -0700

    added brainstorming doc on database model and added a couple entries to MenuItem table.

--- SQL/PopulateDatabase.sql
+++ SQL/PopulateDatabase.sql
@@ -48,11 +48,13 @@ INSERT INTO Menu.FamilyDinner(Label, MinNumOrder, MinNumOrderForSpecial) VALUES 
 --MenuItem
 INSERT INTO Menu.MenuItem(Label, SubLabel, Price, CategoryId, CanBeSpicy, IsSpicyByDefault, DefaultSpicyOptionId, IsAvailable, FamilyDinnerId)
 	VALUES 
-	('Golden Crown Appetizer', '(parchment chicken, B.B.Q pork, fried wonton, fried shrimp, shrimp egg roll)', 9.25, 
+	('Golden Crown Appetizer', '(parchment chicken, BBQ pork, fried wonton, fried shrimp, shrimp egg roll)', 9.25, 
 		(SELECT CategoryId FROM Menu.Category WHERE Label = 'Appetizers'), 1, 0, NULL, 1, NULL),
 	('BBQ Pork', NULL, 7.25,
 		(SELECT CategoryId FROM Menu.Category WHERE Label = 'Appetizers'), 1, 0, NULL, 1, NULL),
-	('Sesame Flyboy', '(8)', 7.25,
+	('Sesame Flyboy', '8 total', 7.25,
+		(SELECT CategoryId FROM Menu.Category WHERE Label = 'Appetizers'), 1, 0, NULL, 1, NULL),
+	('Small Appetizer', 'B.B.Q pork, sesame flyboy, fried wonton', 7.50,
 		(SELECT CategoryId FROM Menu.Category WHERE Label = 'Appetizers'), 1, 0, NULL, 1, NULL);
 	
-
+	SELECT Label, SubLabel, Price, CategoryId, CanBeSpicy, IsSpicyByDefault, DefaultSpicyOptionId, IsAvailable, FamilyDinnerId FROM Menu.MenuItem;
