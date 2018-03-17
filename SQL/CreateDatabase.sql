27d2733a8dd99a358724e276fdd8c15e778a49a9
Jonathan Elliott <jonathan.elliott1247@gmail.com>
2018-03-17 12:19:39 -0700

    Made check constraint and associated user defined function for one special per familydinner.

--- SQL/CreateDatabase.sql
+++ SQL/CreateDatabase.sql
@@ -2,8 +2,8 @@ USE GoldenCrownSalem;
 
 --Defensive cleanup
 DROP TABLE IF EXISTS Menu.FamilyDinnerMenuItem;
-DROP TABLE IF EXISTS Menu.MenuItem;
 DROP TABLE IF EXISTS Menu.FamilyDinner;
+DROP TABLE IF EXISTS Menu.MenuItem;
 DROP TABLE IF EXISTS Menu.SpicyOption;
 DROP TABLE IF EXISTS Menu.Category;
 
@@ -16,6 +16,9 @@ IF OBJECT_ID (N'Menu.SpicyOptionId', N'FN') IS NOT NULL
 IF OBJECT_ID (N'Menu.FamilyDinnerId', N'FN') IS NOT NULL  
     DROP FUNCTION Menu.FamilyDinnerId;
 
+IF OBJECT_ID (N'Menu.OneSpecialPerFamilyDinnerFunc', N'FN') IS NOT NULL  
+    DROP FUNCTION Menu.OneSpecialPerFamilyDinnerFunc;
+
 DROP SCHEMA IF EXISTS Menu;
 GO
 
@@ -25,9 +28,8 @@ GO
 
 CREATE TABLE Menu.Category
 (
-	CategoryId	INT IDENTITY(1,1) PRIMARY KEY,
-	Label			VARCHAR(100) UNIQUE NOT NULL,
-	SubLabel		VARCHAR(100)
+	CategoryId		INT IDENTITY(1,1) PRIMARY KEY,
+	Label			VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE Menu.SpicyOption
@@ -36,13 +38,6 @@ CREATE TABLE Menu.SpicyOption
 	Label			VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE Menu.FamilyDinner
-(
-	FamilyDinnerId			INT IDENTITY(1,1) PRIMARY KEY,
-	Description				VARCHAR(100) UNIQUE NOT NULL,
-	MinNumOrder				INT NOT NULL,
-	MinNumOrderForSpecial	INT NOT NULL
-);
 
 CREATE TABLE Menu.MenuItem
 (
@@ -53,22 +48,43 @@ CREATE TABLE Menu.MenuItem
 	IsAvailable				BIT,
 	CategoryId				INT FOREIGN KEY REFERENCES Menu.Category(CategoryId),
 	DefaultSpicyOptionId	INT FOREIGN KEY REFERENCES Menu.SpicyOption(SpicyOptionId),
-	FamilyDinnerId			INT FOREIGN KEY REFERENCES Menu.FamilyDinner(FamilyDinnerId),
 
 	CONSTRAINT UniqueLabel	UNIQUE(Label, SubLabel)
 	
 );
 
+CREATE TABLE Menu.FamilyDinner
+(
+	FamilyDinnerId			INT IDENTITY(1,1) PRIMARY KEY,
+	MenuItemId				INT FOREIGN KEY REFERENCES Menu.MenuItem(MenuItemId),
+	MinNumOrder				INT NOT NULL,
+	MinNumOrderForSpecial	INT NOT NULL
+);
 
 CREATE TABLE Menu.FamilyDinnerMenuItem
 (
 	FamilyDinnerMenuItemId			INT IDENTITY(1,1) PRIMARY KEY,
 	FamilyDinnerId					INT FOREIGN KEY REFERENCES Menu.FamilyDinner(FamilyDinnerId) NOT NULL,
 	MenuItemId						INT FOREIGN KEY REFERENCES Menu.MenuItem(MenuItemId) NOT NULL,
+	Label							VARCHAR(100),
+	IsSpecial						BIT NOT NULL
 );
 
+GO
+CREATE FUNCTION Menu.OneSpecialPerFamilyDinnerFunc(@FamilyDinnerId VARCHAR(100))
+RETURNS INT   
+AS   
+BEGIN
+	RETURN (SELECT COUNT(*) FROM Menu.FamilyDInnerMenuItem WHERE IsSpecial = 1 AND FamilyDinnerId = @FamilyDinnerId);
+END; 
+GO
+
+ALTER TABLE Menu.FamilyDinnerMenuItem
+ADD CONSTRAINT OneSpecialPerFamilyDinner CHECK(	(IsSpecial = 0)
+											OR	(Menu.OneSpecialPerFamilyDinnerFunc(FamilyDinnerId) = 0))
+
 --*********************************************************************************************************************
---<Helper user defined functions>
+--<PopulateDatabase.sql script helper user defined functions>
 --*********************************************************************************************************************
 GO
 CREATE FUNCTION Menu.CategoryId(@CategoryLabel VARCHAR(100))  
@@ -98,5 +114,5 @@ END;
 GO
 
 --*********************************************************************************************************************
---</Helper user defined functions>
+--</PopulateDatabase.sql script helper user defined functions>
 --*********************************************************************************************************************
