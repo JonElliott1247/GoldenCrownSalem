dfc1303941ba13d41efcb1a033c6f2a2bdb3446c
Jonathan Elliott <jonathan.elliott1247@gmail.com>
2018-04-24 01:26:22 -0700

    Added tables for user accounts and orders.

--- SQL/CreateDatabase.sql
+++ SQL/CreateDatabase.sql
@@ -6,6 +6,7 @@ BEGIN TRANSACTION [Cleanup]
 	DROP TABLE IF EXISTS Menu.CombinationPlateItem;
 	DROP TABLE IF EXISTS Menu.MenuItem_FamilyDinnerItem;
 	DROP TABLE IF EXISTS Menu.FamilyDinnerItem;
+	DROP TABLE IF EXISTS Sales.OrderItem;
 	DROP TABLE IF EXISTS Menu.MenuItem;
 	DROP TABLE IF EXISTS Menu.SpicyOption;
 	DROP TABLE IF EXISTS Menu.Category;
@@ -32,6 +33,17 @@ BEGIN TRANSACTION [Cleanup]
 		DROP FUNCTION Menu.CombinationPlateItemId;
 
 	DROP SCHEMA IF EXISTS Menu;
+	
+	DROP TABLE IF EXISTS Sales.[Order];
+	DROP TABLE IF EXISTS Sales.Account;
+
+	IF OBJECT_ID (N'Sales.OrderSubTotal', N'FN') IS NOT NULL
+		DROP FUNCTION Sales.OrderSubTotal;
+
+	IF OBJECT_ID (N'Sales.OrderItemSubTotal', N'FN') IS NOT NULL
+		DROP FUNCTION Sales.OrderItemSubTotal;
+
+	DROP SCHEMA IF EXISTS Sales;
 
 COMMIT TRANSACTION [Cleanup]
 GO
@@ -194,3 +206,48 @@ GO
 --</PopulateDatabase.sql script helper user defined functions>
 --*********************************************************************************************************************
 
+CREATE SCHEMA Sales;
+GO
+
+CREATE TABLE Sales.Account
+(
+	AccountId	INT IDENTITY(1,1) PRIMARY KEY,
+	Name		VARCHAR(50),
+	Salt		BINARY(16),
+	Hash		BINARY(36)
+);
+
+GO
+CREATE FUNCTION Sales.OrderSubTotal(@OrderId INT)
+RETURNS MONEY
+AS
+BEGIN
+	RETURN (SELECT SUM(SubTotal) FROM Sales.OrderItem AS OrderItem WHERE OrderItem.OrderId = @OrderId)
+END;
+GO
+
+CREATE TABLE Sales.[Order]
+(
+	OrderId		INT IDENTITY(1,1) PRIMARY KEY,
+	AccountId	INT FOREIGN KEY REFERENCES Sales.Account(AccountId) NOT NULL,
+	SubTotal	AS Sales.OrderSubTotal(OrderId),
+	Tip			MONEY DEFAULT 0 NOT NULL
+);
+
+GO
+CREATE FUNCTION Sales.OrderItemSubTotal(@MenuItemId INT, @Quantity INT)
+RETURNS MONEY
+AS
+BEGIN
+	RETURN ((SELECT Price FROM Menu.MenuItem AS MenuItem WHERE MenuItem.MenuItemId = MenuItemId) * @Quantity)
+END;
+GO
+
+CREATE TABLE Sales.OrderItem
+(
+	OrderItemId	INT IDENTITY(1,1) PRIMARY KEY,
+	MenuItemId	INT FOREIGN KEY REFERENCES Menu.MenuItem(MenuItemId) NOT NULL,
+	OrderId		INT FOREIGN KEY REFERENCES Sales.[Order](OrderId) NOT NULL,
+	Quantity	INT DEFAULT 1 NOT NULL,
+	SubTotal	AS Sales.OrderItemSubTotal(MenuItemId, Quantity)
+);
