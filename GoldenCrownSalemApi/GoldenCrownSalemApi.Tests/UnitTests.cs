8b22ee6c83baae40039a4be625adc7f367d11f8f
Jonathan Elliott <jonathan.elliott1247@gmail.com>
2018-07-06 18:36:55 -0700

    Implemented search at /api/menu/search={searchTerm}

--- GoldenCrownSalemApi/GoldenCrownSalemApi.Tests/UnitTests.cs
+++ GoldenCrownSalemApi/GoldenCrownSalemApi.Tests/UnitTests.cs
@@ -11,55 +11,69 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Tests
 {
-    public static class TestHelper
+    public interface ITestHelper
     {
-        private static readonly string _url = "http://localhost:51099";
-        private static readonly GoldenCrownSalemContext _context = new GoldenCrownSalemContext();
-        private static readonly HttpClient _client = new HttpClient();
-
-        public static IList<CategoryViewModel> GetCategoryViewModelsFromApi()
-        {
-            string requestUrl = _url+"/api/menu";
-            string menuCategoriesResponse = _client.GetStringAsync(requestUrl).Result;
-            return JsonConvert.DeserializeObject<List<CategoryViewModel>>(menuCategoriesResponse);
-        }
-
-        public static IList<Category> GetSubSetCategoriesWithMenuItemsUsingContext()
-        {
-            int oneFifthCategoryCount = _context.Category.Count() / 5;
-            int randomNumber = new Random().Next(0, oneFifthCategoryCount);
-            return _context.Category.Where(item => item.CategoryId % oneFifthCategoryCount == randomNumber)
-                                             .Include(item => item.MenuItem).ToList();
-        }
-
-        public static GoldenCrownSalemContext Context() { return _context; }
-
-
-
+        IList<MenuItemViewModel> GetMenuItemsFromApi();
+        IList<MenuItem> GetSubSetMenuItemsUsingContext();
     }
 
 
 
+    public class TestHelper : ITestHelper
+    {
+        private readonly string _url = "http://localhost:51099";
+        private readonly GoldenCrownSalemContext _context = new GoldenCrownSalemContext();
+        private readonly HttpClient _client = new HttpClient();
+
+        public IList<MenuItemViewModel> GetMenuItemsFromApi()
+        {
+            string requestUrl = _url+"/api/menu";
+            string menuItems = _client.GetStringAsync(requestUrl).Result;
+            return JsonConvert.DeserializeObject<List<MenuItemViewModel>>(menuItems);
+        }
+
+        public IList<MenuItem> GetSubSetMenuItemsUsingContext()
+        {
+            int oneFifthCount = _context.MenuItem.Count() / 5;
+            int randomNumber = new Random().Next(0, oneFifthCount);
+            return _context.MenuItem.Where(item => item.MenuItemId % oneFifthCount == randomNumber).ToList();
+        }
+
+    }
+
+
+    
+
     [TestFixture]
     public class ApiShould
     {
+        private readonly TestHelper _helper1 = new TestHelper();
+        ITestHelper Helper => _helper1;
 
         [Test]
-        public void PopulateMenuCategories()
+        public void PopulateMenuItems()
         {
-            var menuCategories = TestHelper.GetCategoryViewModelsFromApi();
-            Assert.That(menuCategories.Count > 1);
+            var menuItems = Helper.GetMenuItemsFromApi();
+            Assert.That(menuItems.Count > 1);
         }
-
-
 
         [Test]
         public void ContainValidMenuCategories()
         {
-            var menuCategories = TestHelper.GetCategoryViewModelsFromApi();
-            var categoryNames = TestHelper.Context().Category.Select(cat => cat.Label).ToList();
-            Assert.That(menuCategories.Where(item => categoryNames.Contains(item.Label)).Count() == categoryNames.Count);
+            var menuItems = Helper.GetMenuItemsFromApi().Select(item => item.Label);
+            var subset = Helper.GetSubSetMenuItemsUsingContext().Select(item => item.Label);
+            Assert.That(subset.Where(item => menuItems.Contains(item)).Count() == subset.Count());
         }
+
+        /*
+
+        [Test]
+        public void PopulateMenuSearch()
+        {
+            var menuItems = Helper.GetMenuItemsFromApi().
+        }
+        */
+
 
         /*
         [Test]
--- GoldenCrownSalemApi/GoldenCrownSalemApi/Controllers/MenuController.cs
+++ GoldenCrownSalemApi/GoldenCrownSalemApi/Controllers/MenuController.cs
@@ -13,7 +13,7 @@ using Microsoft.EntityFrameworkCore;
 namespace GoldenCrownSalemApi.Controllers
 {
     [Produces("application/json")]
-    [Route("api/menu")]
+    [Route("api/[controller]")]
     public class MenuController : Controller
     {
         private readonly IMapper _mapper;
@@ -23,21 +23,75 @@ namespace GoldenCrownSalemApi.Controllers
         }
 
         // GET api/menu
+        // Returns ALL menu items
         [HttpGet]
-        public List<CategoryViewModel> Get()
+        public List<MenuItemViewModel> Get()
         {
-            var list = new List<CategoryViewModel>();
+            var list = new List<MenuItemViewModel>();
             using (var context = new GoldenCrownSalemContext())
             {
-                var categories = context.Category.ToList();
-                foreach(var category in categories)
+                var menuItems = context.MenuItem.Include(item => item.DefaultSpicyOption)
+                                                .Include(item => item.Category).ToList();
+                foreach(var menuItem in menuItems)
                 {
-                    var viewModel = _mapper.Map<CategoryViewModel>(category);
+                    var viewModel = _mapper.Map<MenuItemViewModel>(menuItem);
                     list.Add(viewModel);
                 }
             }
 
             return list;
+        }
+
+        private List<string> Pluralize(string searchTerm)
+        {
+            var terms = new List<String>();
+            terms.Add(searchTerm.ToLower());
+            if (searchTerm[searchTerm.Length - 1] == 's')
+            {
+                terms.Add(searchTerm.ToLower().Substring(0, searchTerm.Length - 2));
+            }
+            else
+            {
+                terms.Add(searchTerm.ToLower() + 's');
+            }
+
+            return terms;
+        }
+
+        private bool StringContainsOneOrMore(string text, IList<string> list)
+        {
+            bool containsOneOrMore = false;
+            foreach(var item in list)
+            {
+                if(text.Contains(item.Trim()))
+                {
+                    containsOneOrMore = true;
+                }
+            }
+            return containsOneOrMore;
+        }
+
+        // api/menu/search=noodles
+        [HttpGet("search={searchTerm}")]
+        public List<MenuItemViewModel> GetSearch(string searchTerm)
+        {
+
+            var list = new List<MenuItemViewModel>();
+            using (var context = new GoldenCrownSalemContext())
+            {
+
+                var menuItems = context.MenuItem.Include(item => item.DefaultSpicyOption).Include(item => item.Category)
+                                                .Where(item => StringContainsOneOrMore(item.Label.ToLower(), Pluralize(searchTerm)))
+                                                .ToList();
+
+                foreach (var item in menuItems)
+                {
+                    var viewModel = _mapper.Map<MenuItemViewModel>(item);
+                    list.Add(viewModel);
+                }
+
+                return list;
+            }
         }
 
         // GET api/menu/chow-mein
@@ -58,6 +112,8 @@ namespace GoldenCrownSalemApi.Controllers
             }
             return list;
         }
+
+
 
 
     }
