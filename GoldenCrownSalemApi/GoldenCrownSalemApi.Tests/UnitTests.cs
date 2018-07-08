07616f353ea545be80414056c89cce9d16482370
Jonathan Elliott <jonathan.elliott1247@gmail.com>
2018-07-08 12:50:32 -0700

    Added unit test for new search feature for menu items.

--- GoldenCrownSalemApi/GoldenCrownSalemApi.Tests/UnitTests.cs
+++ GoldenCrownSalemApi/GoldenCrownSalemApi.Tests/UnitTests.cs
@@ -13,11 +13,11 @@ namespace Tests
 {
     public interface ITestHelper
     {
-        IList<MenuItemViewModel> GetMenuItemsFromApi();
+        IList<MenuItemViewModel> GetMenuItemsFromApi(string requestUrl);
         IList<MenuItem> GetSubSetMenuItemsUsingContext();
+        MenuItem GetRandomMenuItemFromContext();
+        string GetValidSearchTerm();
     }
-
-
 
     public class TestHelper : ITestHelper
     {
@@ -25,9 +25,8 @@ namespace Tests
         private readonly GoldenCrownSalemContext _context = new GoldenCrownSalemContext();
         private readonly HttpClient _client = new HttpClient();
 
-        public IList<MenuItemViewModel> GetMenuItemsFromApi()
+        public IList<MenuItemViewModel> GetMenuItemsFromApi(string requestUrl)
         {
-            string requestUrl = _url+"/api/menu";
             string menuItems = _client.GetStringAsync(requestUrl).Result;
             return JsonConvert.DeserializeObject<List<MenuItemViewModel>>(menuItems);
         }
@@ -39,6 +38,19 @@ namespace Tests
             return _context.MenuItem.Where(item => item.MenuItemId % oneFifthCount == randomNumber).ToList();
         }
 
+        public MenuItem GetRandomMenuItemFromContext()
+        {
+            var list = _context.MenuItem.ToList();
+            int randomNumber = new Random().Next(0, list.Count());
+            return list[randomNumber];
+        }
+
+        public string GetValidSearchTerm()
+        {
+            string[] terms = GetRandomMenuItemFromContext().Label.Trim().Split(" ");
+            int randomNumber = new Random().Next(0, terms.Length);
+            return terms[randomNumber];
+        }
     }
 
 
@@ -47,32 +59,36 @@ namespace Tests
     [TestFixture]
     public class ApiShould
     {
+        private static readonly string _url = "http://localhost:51099";
+        private static readonly string _menuUrl = _url + "/api/menu";
+        private static readonly string _searchMenuUrl = _menuUrl + "/search=";
+
         private readonly TestHelper _helper1 = new TestHelper();
         ITestHelper Helper => _helper1;
 
         [Test]
         public void PopulateMenuItems()
         {
-            var menuItems = Helper.GetMenuItemsFromApi();
-            Assert.That(menuItems.Count > 1);
+            var menuItems = Helper.GetMenuItemsFromApi(_menuUrl);
+            Assert.That(menuItems.Count > 0);
         }
 
         [Test]
         public void ContainValidMenuCategories()
         {
-            var menuItems = Helper.GetMenuItemsFromApi().Select(item => item.Label);
+            var menuItems = Helper.GetMenuItemsFromApi(_menuUrl).Select(item => item.Label);
             var subset = Helper.GetSubSetMenuItemsUsingContext().Select(item => item.Label);
             Assert.That(subset.Where(item => menuItems.Contains(item)).Count() == subset.Count());
         }
 
-        /*
-
         [Test]
-        public void PopulateMenuSearch()
+        public void PopulateMenuItemSearch()
         {
-            var menuItems = Helper.GetMenuItemsFromApi().
+            string searchTerm = Helper.GetValidSearchTerm();
+            var menuItems = Helper.GetMenuItemsFromApi(_searchMenuUrl + searchTerm);
+            Assert.That(menuItems.Count() > 1);
         }
-        */
+        
 
 
         /*
