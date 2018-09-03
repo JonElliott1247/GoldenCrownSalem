2e06c4a19a607ad21ddac14c559da80ae5791c1d
Jonathan Elliott <jonathan.elliott1247@gmail.com>
2018-09-03 13:52:49 -0700

    Removing AccountId property in Account dtos.

--- GoldenCrownSalemApi/GoldenCrownSalemApi/Models/Dtos/AccountGetDto.cs
+++ GoldenCrownSalemApi/GoldenCrownSalemApi/Models/Dtos/AccountGetDto.cs
@@ -7,7 +7,6 @@ namespace GoldenCrownSalemApi.Models.Dtos
 {
     public class AccountGetDto
     {
-        public int AccountId { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
